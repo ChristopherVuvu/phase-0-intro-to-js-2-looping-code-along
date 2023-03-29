ce61ee00f035c2ee293c49a1c49f3541753a51cb
ChristopherVuvu <vuvumumo93@gmail.com>
2023-03-29 20:16:23 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const names =["James", "Isaac", "Winfred"];
+const event = "birthday";
+
+function writeCards(names, event){
+    const messages = [];
+    for(let i=0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]} for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+//console.log(writeCards(names, event));
+
+
+function countDown(num){
+    while (num >= 0){
+    console.log(num);
+    num--;
+    }
+}
+countDown(10);
